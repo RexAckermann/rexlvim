28cd824411edc9e1884ef763646e4c9913c9b89c
Rex Ackermann <61972253+RexAckermann@users.noreply.github.com>
2023-05-01 00:34:44 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/env bash
+
 cd
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) 
 curl -fsSl https://raw.githubusercontent.com/RexAckermann/rexlvim/main/installlvim | bash
